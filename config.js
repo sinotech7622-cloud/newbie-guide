82395d9694f6a24dfa68881d7f1053dc8a61293b
sinotech7622-cloud <sinotech7622@gmail.com>
2026-09-22 12:08:26 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,21 +1,71 @@
 /* 園區及城鄉發展部｜新人冒險指南 — 連線設定
-   請到網站的管理者專區（網址後加 #admin）→「設定小幫手」產生這個檔案的內容，再上傳到 GitHub 取代本檔。
-   這個檔案不含管理密碼。還沒設定前，網站仍可正常遊玩，只是不會回報進度，也不能收發面談通知與技術諮詢。 */
+   這個檔案由「管理者專區 → 設定小幫手」產生（網址後加 #admin 進入）。
+   不含管理密碼；表單或試算表變更時，重新產生再覆蓋即可。 */
 window.SINO22_CONFIG = {
   "forms": {
-    "progress": { "action": "", "fields": {} },
-    "consult":  { "action": "", "fields": {} },
-    "ivReply":  { "action": "", "fields": {} },
-    "admin":    { "action": "", "fields": {} }
+    "progress": {
+      "action": "https://docs.google.com/forms/d/e/1FAIpQLSfKPmxuW1ZEy57PKDbHy1J5gSfxpd-JeJl0tF8KGOCQAZvwPQ/formResponse",
+      "fields": {
+        "code": "1816567401",
+        "name": "430075367",
+        "group": "1337882985",
+        "level": "1142750946",
+        "progress": "491493452",
+        "awarded": "1678304642",
+        "detail": "1848604557",
+        "empno": "1598263298"
+      }
+    },
+    "consult": {
+      "action": "https://docs.google.com/forms/d/e/1FAIpQLSf6MHryW7Pj87hnYFTj2nKWuFehdY_OsFBqoXi4x564NgiVOQ/formResponse",
+      "fields": {
+        "no": "383617001",
+        "code": "1302029120",
+        "name": "335899886",
+        "group": "896892579",
+        "category": "486445359",
+        "topic": "1736138049",
+        "detail": "625677698",
+        "preferred": "1638398402",
+        "contact": "640523815",
+        "empno": "123098348"
+      }
+    },
+    "ivReply": {
+      "action": "https://docs.google.com/forms/d/e/1FAIpQLSdEHFCPbIgmDmcvCeRveMIPPj7bpLz-MOjkc_cLK2ZmPfPIyQ/formResponse",
+      "fields": {
+        "notice": "700492093",
+        "code": "188722457",
+        "name": "528458140",
+        "status": "1595171812",
+        "message": "1873072730",
+        "empno": "545739212"
+      }
+    },
+    "admin": {
+      "action": "https://docs.google.com/forms/d/e/1FAIpQLScxiYinh7TXbXLCbaQC6NHNkar3od2v_JmXaKRXsdPvBevYCw/formResponse",
+      "fields": {
+        "pass": "1160307412",
+        "type": "2016103781",
+        "id": "513627691",
+        "part": "1866959681",
+        "data": "297146273",
+        "by": "1406271927"
+      }
+    }
   },
   "sheets": {
-    "feed": ""
+    "feed": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQn_We5-2mBM6CDNzeTt1cJDqbF3P-x5OItfrQ04S_bLtf0rDdvfX-piEjib_6iVjdQe0gHwxTPz08Y/pub?gid=592103693&single=true&output=csv",
+    "settings": "",
+    "interviews": "",
+    "replies": "",
+    "content": ""
   },
   "links": {
-    "spreadsheet": "",
-    "progressForm": "",
-    "consultForm": "",
-    "ivReplyForm": "",
-    "adminForm": ""
+    "spreadsheet": "https://docs.google.com/spreadsheets/d/1DVl74v-PBQ2pIEfYD2kHrY0zHc9kHEDuAiErdFZI-RI/edit?usp=drive_link",
+    "progressForm": "https://docs.google.com/forms/d/12sFwaUhAmRv6XGRmp-8_KPtOe73_hi7dKnUpRbp00w8/edit",
+    "consultForm": "https://docs.google.com/forms/d/1xWOq_gAXxpHCX932Dnf2wlq4zXLhRSZU75k5ovpjhUw/edit",
+    "ivReplyForm": "https://docs.google.com/forms/d/1XWTTFtypSjPa78Y0FMja2a-5D8e6Jm3v7rDIxNAI1Ac/edit",
+    "adminForm": "https://docs.google.com/forms/d/1Jm00FG7dN4B4M_YBMrqQAN-6ltGJsdJkXvQSmtuKdTc/edit"
   }
 };
